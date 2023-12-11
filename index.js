bb1e3fc6ebb6963d2141e1ce97f83d1183350ab6
Samer Mobarak <samermobarak888@gmail.com>
2023-12-11 10:59:47 -0500

    algorithms

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// Helper function to clean the string (remove non-letter characters and convert to lowercase)
+function cleanString(str) {
+  return str.replace(/[^a-z]/g, '').toLowerCase();
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+// Main function to check if a string is a palindrome
+function isPalindrome(str) {
+  const cleanedStr = cleanString(str);
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
 }
+
+// Example test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+
 
 module.exports = isPalindrome;
